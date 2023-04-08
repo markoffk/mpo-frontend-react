708ec6b035c0d71deba9bd80057e96572311cb98
Vlad Zayats <1987vlad>
2023-04-08 10:27:41 +0200

    commit

--- src/generateCalendarEvents.ts
+++ src/generateCalendarEvents.ts
@@ -1,6 +1,38 @@
+import { datetime, RRule, RRuleSet, rrulestr, ByWeekday } from "rrule";
 import { EventAttributes } from "ics";
 import dayjs from "dayjs";
 import { StreetSchedule } from "./types";
+
+type WasteType = keyof StreetSchedule["waste"];
+
+const polishDayToRRuleDayMap: { [key: string]: ByWeekday } = {
+    poniedziałek: RRule.MO,
+    wtorek: RRule.TU,
+    środa: RRule.WE,
+    czwartek: RRule.TH,
+    piątek: RRule.FR,
+    sobota: RRule.SA,
+    niedziela: RRule.SU,
+};
+const polishDayToEnglishDayMap = {
+    poniedziałek: "Monday",
+    wtorek: "Tuesday",
+    środa: "Wednesday",
+    czwartek: "Thursday",
+    piątek: "Friday",
+    sobota: "Saturday",
+    niedziela: "Sunday",
+};
+const polishDays = Object.keys(polishDayToEnglishDayMap);
+
+const wasteTypeMap: { [key in WasteType]: string } = {
+    mixed: "zmieszane",
+    paper: "papier",
+    plastic: "tworzywa sztuczne",
+    glass: "szkło",
+    bio: "bio",
+    barrel: "beczka",
+};
 
 const splitDates = (dates: string) => {
     return (dates ?? "")
@@ -13,30 +45,58 @@ const generateEvent = (
     year: number,
     month: number,
     day: number,
-    wasteType: string,
+    wasteType: WasteType,
     street: string,
     houseNumber: string
 ): EventAttributes => {
-    const targetDate = dayjs(`${year}-${month}-${day}`);
-    const nextDate = targetDate.add(1, "day");
-
     return {
         start: [year, month, day],
-        end: [nextDate.year(), nextDate.month() + 1, nextDate.date()],
-        title: `Odbior odpadów - ${wasteType}`,
-        description: wasteType,
+        duration: { days: 1 },
+        title: `Odbior odpadów - ${wasteTypeMap[wasteType]}`,
+        description: wasteTypeMap[wasteType],
         location: `${street} ${houseNumber}`,
         url: "http://zayats.pl",
         categories: ["Odbior odpadów"],
         status: "CONFIRMED",
         busyStatus: "FREE",
+        calName: "vlad test",
+        uid: btoa(`mpo.zayats.pl-${year}-${month}`),
+        organizer: { name: "Władysław Zayats", email: "easypaste.org@gmail.com" },
+        alarms: [{ action: "display", description: "Reminder", trigger: { minutes: 10, before: true } }],
     };
 };
 
-export const generateCalendarEventsForOneWasteType = (schedule: StreetSchedule, wasteType: string, dates: string[]) => {
+const generateEventByRRule = (
+    rrule: string,
+    year: number,
+    wasteType: WasteType,
+    street: string,
+    houseNumber: string
+): EventAttributes => {
+    return {
+        recurrenceRule: rrule.slice(6),
+        start: [year, 1, 1],
+        duration: { days: 1 },
+        title: `Odbior odpadów - ${wasteTypeMap[wasteType]}`,
+        description: wasteTypeMap[wasteType],
+        location: `${street} ${houseNumber}`,
+        url: "http://zayats.pl",
+        categories: ["Odbior odpadów"],
+        status: "CONFIRMED",
+        busyStatus: "FREE",
+        calName: "vlad test",
+        organizer: { name: "Władysław Zayats", email: "easypaste.org@gmail.com" },
+        alarms: [{ action: "display", description: "Reminder", trigger: { minutes: 10, before: true } }],
+    };
+};
+
+export const generateCalendarEventsForOneWasteType = (schedule: StreetSchedule, wasteType: WasteType) => {
     const events = [];
 
+    const dates = splitDates(schedule.waste[wasteType]);
+
     if (dates.every((date: string) => new RegExp("^[0-9]{2}.[0-9]{2}$").test(date))) {
+        console.log("aaa");
         for (const date of dates) {
             const [day, month] = date.split(".");
 
@@ -51,17 +111,33 @@ export const generateCalendarEventsForOneWasteType = (schedule: StreetSchedule, 
                 )
             );
         }
+    } else if (dates.every((date: string) => polishDays.includes(date.toLocaleLowerCase("pl")))) {
+        const dtStart = dayjs(`${schedule.year}-01-01`);
+
+        const rrule = new RRule({
+            until: dtStart.endOf("year").toDate(),
+            freq: RRule.WEEKLY,
+            byweekday: dates.map((date) => polishDayToRRuleDayMap[date]),
+        });
+
+        events.push(
+            generateEventByRRule(rrule.toString(), schedule.year, wasteType, schedule.street, schedule.houseNumber)
+        );
     }
 
     return events;
 };
 export const generateCalendarEvents = (schedule: StreetSchedule): EventAttributes[] => {
+    // return [
+    //     generateEvent(2023, 4, 3, "mixed", "Widłakowa", "56c"),
+    //     generateEvent(2023, 4, 4, "mixed", "Widłakowa", "56c"),
+    // ];
     return [
-        ...generateCalendarEventsForOneWasteType(schedule, "zmieszane", splitDates(schedule.waste.mixed)),
-        ...generateCalendarEventsForOneWasteType(schedule, "papier", splitDates(schedule.waste.paper)),
-        ...generateCalendarEventsForOneWasteType(schedule, "tworzywa sztuczne", splitDates(schedule.waste.plastic)),
-        ...generateCalendarEventsForOneWasteType(schedule, "szkło", splitDates(schedule.waste.glass)),
-        ...generateCalendarEventsForOneWasteType(schedule, "bio", splitDates(schedule.waste.bio)),
-        ...generateCalendarEventsForOneWasteType(schedule, "beczka", splitDates(schedule.waste.barrel)),
+        ...generateCalendarEventsForOneWasteType(schedule, "mixed"),
+        ...generateCalendarEventsForOneWasteType(schedule, "paper"),
+        ...generateCalendarEventsForOneWasteType(schedule, "plastic"),
+        ...generateCalendarEventsForOneWasteType(schedule, "glass"),
+        ...generateCalendarEventsForOneWasteType(schedule, "bio"),
+        ...generateCalendarEventsForOneWasteType(schedule, "barrel"),
     ];
 };
