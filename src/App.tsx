5e31d2ad63ad8726ff3612d030c2f7dc3b21f49f
Vlad Zayats <1987vlad>
2023-04-10 09:39:55 +0200

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import {
+    AppBar,
     Autocomplete,
     Button,
+    Container,
     Paper,
     Stack,
     Table,
@@ -9,7 +11,10 @@ import {
     TableContainer,
     TableRow,
     TextField,
+    Toolbar,
+    Typography,
 } from "@mui/material";
+import RecyclingIcon from "@mui/icons-material/Recycling";
 import { useEffect, useMemo, useState } from "react";
 import { StreetSchedule } from "./types";
 import { generateCalendarFile } from "./generateCalendarFile";
@@ -91,115 +96,127 @@ function App() {
     }, [selectedStreet]);
 
     return (
-        <Stack sx={{ width: "100%", height: "100vh", padding: "50px" }} gap={2} alignItems="center">
-            <Autocomplete
-                value={selectedStreet}
-                onChange={(event, newValue) => {
-                    setSelectedStreet(newValue);
-                }}
-                disablePortal
-                options={streets}
-                sx={{ width: "100%", maxWidth: 500 }}
-                renderInput={(params) => <TextField {...params} label="Ulica" />}
-            />
-            <Autocomplete
-                disabled={streetSchedules.length === 0}
-                value={selectedStreetSchedule}
-                isOptionEqualToValue={(option, value) => option.id === value.id}
-                onChange={(event, newValue) => {
-                    setSelectedStreetSchedule(newValue);
-                }}
-                getOptionLabel={(option) => option.houseNumber}
-                disablePortal
-                options={streetSchedules}
-                sx={{ width: "100%", maxWidth: 500 }}
-                renderOption={(props, option) => {
-                    return (
-                        <li {...props} key={option.id}>
-                            {option.houseNumber}
-                        </li>
-                    );
-                }}
-                renderInput={(params) => <TextField {...params} label="Numer" />}
-            />
+        <>
+            <AppBar position="static" elevation={0}>
+                <Toolbar>
+                    <RecyclingIcon sx={{ mr: 2, animation: 'rotation 10s infinite linear' }} />
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        Odbiór odpadów MPO Kraków
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Container>
+                <Stack sx={{ width: "100%", padding: "50px" }} gap={2} alignItems="center">
+                    <Autocomplete
+                        value={selectedStreet}
+                        onChange={(event, newValue) => {
+                            setSelectedStreet(newValue);
+                        }}
+                        disablePortal
+                        options={streets}
+                        sx={{ width: "100%", maxWidth: 500 }}
+                        renderInput={(params) => <TextField {...params} label="Ulica" />}
+                    />
+                    <Autocomplete
+                        disabled={streetSchedules.length === 0}
+                        value={selectedStreetSchedule}
+                        isOptionEqualToValue={(option, value) => option.id === value.id}
+                        onChange={(event, newValue) => {
+                            setSelectedStreetSchedule(newValue);
+                        }}
+                        getOptionLabel={(option) => option.houseNumber}
+                        disablePortal
+                        options={streetSchedules}
+                        sx={{ width: "100%", maxWidth: 500 }}
+                        renderOption={(props, option) => {
+                            return (
+                                <li {...props} key={option.id}>
+                                    {option.houseNumber}
+                                </li>
+                            );
+                        }}
+                        renderInput={(params) => <TextField {...params} label="Numer" />}
+                    />
 
-            {selectedStreet && selectedStreetSchedule && (
-                <TableContainer component={Paper} sx={{ width: "100%", maxWidth: 500 }}>
-                    <Table aria-label="simple table">
-                        <TableBody>
-                            <TableRow>
-                                <TableCell component="th" scope="row">
-                                    Typ
-                                </TableCell>
-                                <TableCell align="right">{selectedStreetSchedule.houseType}</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell component="th" scope="row">
-                                    Sektor
-                                </TableCell>
-                                <TableCell align="right">{selectedStreetSchedule.sector}</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell component="th" scope="row">
-                                    Operator
-                                </TableCell>
-                                <TableCell align="right">{selectedStreetSchedule.operator}</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell component="th" scope="row">
-                                    Zmieszane
-                                </TableCell>
-                                <TableCell align="right">{selectedStreetSchedule.waste.mixed}</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell component="th" scope="row">
-                                    Papier
-                                </TableCell>
-                                <TableCell align="right">{selectedStreetSchedule.waste.paper}</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell component="th" scope="row">
-                                    Tworzywa sztuczne
-                                </TableCell>
-                                <TableCell align="right">{selectedStreetSchedule.waste.plastic}</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell component="th" scope="row">
-                                    Szkło
-                                </TableCell>
-                                <TableCell align="right">{selectedStreetSchedule.waste.glass}</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell component="th" scope="row">
-                                    Bio
-                                </TableCell>
-                                <TableCell align="right">{selectedStreetSchedule.waste.bio}</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell component="th" scope="row">
-                                    Beczka
-                                </TableCell>
-                                <TableCell align="right">{selectedStreetSchedule.waste.barrel}</TableCell>
-                            </TableRow>
-                            <TableRow>
-                                <TableCell component="th" scope="row">
-                                    Kalendarz
-                                </TableCell>
-                                <TableCell align="right">
-                                    <Button
-                                        onClick={() =>
-                                            downloadCalendar(selectedStreet.fileIndex, selectedStreetSchedule)
-                                        }
-                                    >
-                                        Pobierz
-                                    </Button>
-                                </TableCell>
-                            </TableRow>
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-            )}
-        </Stack>
+                    {selectedStreet && selectedStreetSchedule && (
+                        <TableContainer component={Paper} sx={{ width: "100%", maxWidth: 500 }}>
+                            <Table aria-label="simple table">
+                                <TableBody>
+                                    <TableRow>
+                                        <TableCell component="th" scope="row">
+                                            Typ
+                                        </TableCell>
+                                        <TableCell align="right">{selectedStreetSchedule.houseType}</TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell component="th" scope="row">
+                                            Sektor
+                                        </TableCell>
+                                        <TableCell align="right">{selectedStreetSchedule.sector}</TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell component="th" scope="row">
+                                            Operator
+                                        </TableCell>
+                                        <TableCell align="right">{selectedStreetSchedule.operator}</TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell component="th" scope="row">
+                                            Zmieszane
+                                        </TableCell>
+                                        <TableCell align="right">{selectedStreetSchedule.waste.mixed}</TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell component="th" scope="row">
+                                            Papier
+                                        </TableCell>
+                                        <TableCell align="right">{selectedStreetSchedule.waste.paper}</TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell component="th" scope="row">
+                                            Tworzywa sztuczne
+                                        </TableCell>
+                                        <TableCell align="right">{selectedStreetSchedule.waste.plastic}</TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell component="th" scope="row">
+                                            Szkło
+                                        </TableCell>
+                                        <TableCell align="right">{selectedStreetSchedule.waste.glass}</TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell component="th" scope="row">
+                                            Bio
+                                        </TableCell>
+                                        <TableCell align="right">{selectedStreetSchedule.waste.bio}</TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell component="th" scope="row">
+                                            Beczka
+                                        </TableCell>
+                                        <TableCell align="right">{selectedStreetSchedule.waste.barrel}</TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell component="th" scope="row">
+                                            Kalendarz
+                                        </TableCell>
+                                        <TableCell align="right">
+                                            <Button
+                                                onClick={() =>
+                                                    downloadCalendar(selectedStreet.fileIndex, selectedStreetSchedule)
+                                                }
+                                            >
+                                                Pobierz
+                                            </Button>
+                                        </TableCell>
+                                    </TableRow>
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    )}
+                </Stack>
+            </Container>
+        </>
     );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import './global.css'
 import App from "./App";
 
-export const muiTheme = createTheme({});
+export const muiTheme = createTheme({
+palette: {
+    primary: {
+        main: '#0063af'
+    }
+}
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
