0354a38305beaa663e357905f884570837e1827c
Vlad Zayats <1987vlad>
2023-04-10 21:01:32 +0200

    commit

--- src/views/schedule/ScheduleView.tsx
+++ src/views/schedule/ScheduleView.tsx
@@ -3,10 +3,15 @@ import { useEffect, useState } from "react";
 import { api } from "../../api";
 import { StreetSchedule } from "../../types";
 import { generateCalendarEventsForPreview } from "../../generateCalendarEvents";
+import { Container, Stack } from "@mui/material";
+import { DateCalendar, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { RRule } from "rrule";
 
 export const ScheduleView = () => {
     const { year, streetIndex, scheduleId } = useParams();
     const [selectedStreetSchedule, setSelectedStreetSchedule] = useState<StreetSchedule | null>(null);
+    const [dates, setDates] = useState<{ [key: string]: Date[] }>({});
 
     useEffect(() => {
         if (year !== undefined && streetIndex !== undefined && scheduleId !== undefined) {
@@ -34,9 +39,27 @@ export const ScheduleView = () => {
         }
     }, [year, streetIndex, scheduleId]);
 
-    if (selectedStreetSchedule) {
-        console.log(generateCalendarEventsForPreview(Number(streetIndex), selectedStreetSchedule));
-    }
+    useEffect(() => {
+        if (selectedStreetSchedule) {
+            const events = generateCalendarEventsForPreview(Number(streetIndex), selectedStreetSchedule);
+            const rrule = RRule.fromString(events[0].events[0].recurrenceRule!);
+            setDates({
+                [events[0].type]: rrule.all(),
+            });
+        }
+    }, [selectedStreetSchedule]);
 
-    return <></>;
+    console.log("dates", dates);
+
+    return (
+        <>
+            <Container>
+                <Stack sx={{ width: "100%", padding: "50px" }} gap={2} alignItems="center">
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                        <DateCalendar />
+                    </LocalizationProvider>
+                </Stack>
+            </Container>
+        </>
+    );
 };
